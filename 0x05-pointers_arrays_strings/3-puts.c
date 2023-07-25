555dab082278f3a193c051917b3a15d3fb19c5db
Rainey254 <reinykenya@gmail.com>
2023-07-25 07:54:49 -0700

    3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,15 +1,31 @@
-include "main.h"
+#include "main.h"
 /**
- * _puts - prints a string, followed by anew line stdout
- * @str: string to print
- */
-void _puts(char *str)
+* print_rev - prints a string in reverse
+* @s: string to print
+*/
+
+void print_rev(char *s)
 {
+int len = 0, index = 0;
+len = _strlen(s);
 
-	while (*str != '\0')
+for (index = len - 1; index >= 0; index--)
+_putchar(s[index]);
 
-	{
-		_putchar(*str++);
-	}
-		_putchar('\n');
+_putchar('\n');
+}
+
+/**
+ *_strlen - returns the length of a string
+ * @s: string
+ *Return: returns lenght;
+ */
+int _strlen(char *s)
+{
+int count, inc;
+inc = 0;
+for (count = 0; s[count] != '\0'; count++)
+inc++;
+
+return (inc);
 }
