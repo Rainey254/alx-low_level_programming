1c2d8fc6f320cd4dc6e992bace2a772a4a2bf39f
Rainey254 <reinykenya@gmail.com>
2023-07-27 03:18:26 -0700

    anything

--- 0x06-pointers_arrays_stringsls/0-strcat.c
+++ 0x06-pointers_arrays_stringsls/0-strcat.c
@@ -1,18 +1,24 @@
-#ifndef INV_HOLBERTON_H
-#define INV_HOLBERTON_H
+/**
+ * _strcat - function that concatenates two strings.
+ *
+ * @dest: pointer to destnation input
+ * @src: pointer to source input
+ *
+ * Return: pointer to resulting string @dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	int c, c2;
 
-int _putchar(char);
-char *_strcat(char *dest, char *src);
-char *_strncat(char *dest, char *src, int n);
-char *_strncpy(char *dest, char *src, int n);
-int _strcmp(char *s1, char *s2);
-void reverse_array(int *a, int n);
-char *string_toupper(char *);
-char *cap_string(char *);
-char *leet(char *);
-char *rot13(char *);
-void print_number(int n);
-char *infinite_add(char *n1, char *n2, char *r, int size_r);
-void print_buffer(char *b, int size);
-#endif
+	c = 0;
+	/* find the size of dest array*/
+	while (dest[c])
+		c++;
 
+	/* iterate through each src array value without the null byte*/
+	for (c2 = 0; src[c2] ; c2++)
+	/* append src[c2] to dest[c] while overwritting the null byte in dest*/
+		dest[c++] = src[c2];
+
+	return (dest);
+}
